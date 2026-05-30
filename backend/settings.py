3d54fe6a613a1b478d831a857b4558a6f78026b5
cesaralbertotecun <ctecunl1@miumg.edu.gt>
2026-05-29 20:41:12 -0600

    Add both frontend URLs to CORS_ALLOWED_ORIGINS

--- backend/settings.py
+++ backend/settings.py
@@ -239,6 +239,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3001",
     "http://127.0.0.1:3001",
+    "https://frontend-mundial.onrender.com",
     "https://frontend-pdp7.onrender.com",
 ]
 
