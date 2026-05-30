b6919db986c4ff2e93528f648fcb260758f71514
cesaralbertotecun <ctecunl1@miumg.edu.gt>
2026-05-29 20:32:30 -0600

    Add frontend Render URL to CORS_ALLOWED_ORIGINS

--- backend/settings.py
+++ backend/settings.py
@@ -239,6 +239,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3001",
     "http://127.0.0.1:3001",
+    "https://frontend-pdp7.onrender.com",
 ]
 
 # Agregar URLs de Railway para CORS
